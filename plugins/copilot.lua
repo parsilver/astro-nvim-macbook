be0d8aef9ffa45334c95bf46932da6fae669c466
parsilver <magician_canobi@hotmail.com>
2024-01-26 11:52:45 +0700

    Fix copilot

--- plugins/copilot.lua
+++ plugins/copilot.lua
@@ -1,9 +1,6 @@
 return {
   {
     "github/copilot.vim",
-    event = "InsertEnter",
-    config = function()
-      vim.cmd "source ~/.config/nvim/extra_conf.vim"
-    end,
+    lazy = false,
   },
 }
