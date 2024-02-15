9cd473846108c1e49ea303e45ce7f075bee846c4
parsilver <magician_canobi@hotmail.com>
2024-02-15 15:28:13 +0700

    Add more support

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,9 +7,20 @@ return {
   -- Appearance
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.terminal-integration.vim-tpipeline" },
 
   -- Language packs
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.php" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.json" },
+
+  -- Support package
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
 }
